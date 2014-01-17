c9feee36991129afce65fbd7c1aedb2a33cb2368
mactive <mengqian@imfclub.com>
2014-01-17 18:28:50 +0800

    yibite

--- yibite.js
+++ yibite.js
@@ -1,29 +1,30 @@
 var Crawler = require("crawler").Crawler;
 var urls = [];
 var fs= require('fs');
+var times  = 0;
 
 var c = new Crawler({
-	"maxConnections":10,
+	"maxConnections":1,
 
 	// This will be called for each crawled page
-	"callback":function(error,result,$) {
+	"callback": function(error,result,$) {
 
 	    // $ is a jQuery instance scoped to the server-side DOM of the page
 	    $(".right-intro a.art-myTitle").each(function(index,a) {
-	        c.queue(a.href);
 	        urls.push({
 	        	url: a.href,
-	        	title: a.innerHTML
+	        	title: a.innerHTML,
+	        	no: index
 	        });
 	    });
-	    console.dir(urls);
-	    writeFs(urls);
+	    times += 1;
+	    console.log('call '+times+'times');
 	}
 });
 
 
 function writeFs(urls){
-	var data = {"urls": urls };
+	var data = {"news": urls };
     fs.writeFile('yibite.json', JSON.stringify(data), function (err) {
 	  	if (err) throw err;
 	  	console.log('It\'s saved!');
@@ -31,7 +32,38 @@ function writeFs(urls){
 }
 
 // Queue just one URL, with default callback
-c.queue("http://yibite.node-crawler.com");
+var tasks = [];
+var max = 5;
+for (var i = 1; i < max; i++) {
+	tasks.push({
+		"uri": "http://www.yibite.com/news/index.php?page="+i,
+		"callback": function(error,result,$) {
+			$('.li-holder').each( function(index, div){
+				var timeDiv = $(div).find('.tag-time > span');
+				var linkDiv = $(div).find('.right-intro a.art-myTitle');
+
+				console.log(linkDiv);
+				console.log(linkDiv.innerHTML);
+				
+				urls.push({
+		        	url: linkDiv.href,
+		        	title: linkDiv.innerHTML,
+		        	time: timeDiv.title,
+		        	no: "page"+i+"-"+index
+		        });
+
+			});
+
+		    times += 1;
+		    console.log('queue call '+times+'times');
+	    	writeFs(urls);
+		}
+	});
+};
+
+console.dir(tasks);
+
+c.queue(tasks);
 
 
 // TODO crawler pagination url
