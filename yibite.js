b8366d4a8cce9375852de702ec9dd047e2f5f611
mactive <mactive@gmail.com>
2014-01-17 22:59:34 +0800

    crawl 20pages wow

--- yibite.js
+++ yibite.js
@@ -4,28 +4,13 @@ var fs= require('fs');
 var times  = 0;
 
 var c = new Crawler({
-	"maxConnections":1,
-
-	// This will be called for each crawled page
-	"callback": function(error,result,$) {
-
-	    // $ is a jQuery instance scoped to the server-side DOM of the page
-	    $(".right-intro a.art-myTitle").each(function(index,a) {
-	        urls.push({
-	        	url: a.href,
-	        	title: a.innerHTML,
-	        	no: index
-	        });
-	    });
-	    times += 1;
-	    console.log('call '+times+'times');
-	}
+	"maxConnections": 1,	
 });
 
 
 function writeFs(urls){
 	var data = {"news": urls };
-    fs.writeFile('yibite.json', JSON.stringify(data), function (err) {
+    fs.writeFile("yibite.json", JSON.stringify(data), function (err) {
 	  	if (err) throw err;
 	  	console.log('It\'s saved!');
 	});
@@ -33,29 +18,33 @@ function writeFs(urls){
 
 // Queue just one URL, with default callback
 var tasks = [];
-var max = 5;
-for (var i = 1; i < max; i++) {
+var max = 20;
+for (var i = 1; i <= max; i++) {
 	tasks.push({
-		"uri": "http://www.yibite.com/news/index.php?page="+i,
+		"maxConnections": 1,
+		"uri": "http://yibite.com/news/index.php?page="+i,
 		"callback": function(error,result,$) {
-			$('.li-holder').each( function(index, div){
-				var timeDiv = $(div).find('.tag-time > span');
-				var linkDiv = $(div).find('.right-intro a.art-myTitle');
+			// urls = [];
 
-				console.log(linkDiv);
-				console.log(linkDiv.innerHTML);
-				
+			$('.li-holder').each( function(index, div){
+				var timeDiv = $(div).find('.tags > span.time')[0];
+				var linkDiv = $(div).find('.right-intro > a.art-myTitle')[0];
+				var originDiv = $(div).find('.tags > a.author')[0];
+				var introDiv = $(div).find('.right-intro > span.intro')[0];
+
 				urls.push({
 		        	url: linkDiv.href,
 		        	title: linkDiv.innerHTML,
-		        	time: timeDiv.title,
-		        	no: "page"+i+"-"+index
+		        	time: timeDiv.innerHTML,
+		        	origin: originDiv.innerHTML,
+		        	intro: introDiv.innerHTML,
+		        	no: "page"+times+"-"+index
 		        });
-
+				console.log("page"+times+"-"+index);
 			});
 
 		    times += 1;
-		    console.log('queue call '+times+'times');
+		    console.log('queue call '+times+' times');
 	    	writeFs(urls);
 		}
 	});
